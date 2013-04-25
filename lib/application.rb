cf2522224f60e0a9a535559ee56b4e37a117be18
George <somu@so.mu>
2013-04-25 05:10:01 +0400

    Backing up

--- lib/application.rb
+++ lib/application.rb
@@ -1,6 +1,6 @@
 module Sword
   # Hook-up all gems that we will probably need
-  @settings['gems'].concat(@list).each do |lib|
+  $settings[:gems].concat(@list || []).each do |lib|
     if lib.instance_of? Hash # Take the first possible variant if there are any
       lib.values.flatten.each { |var| begin require var; break rescue LoadError; next end } 
     else begin require lib; rescue LoadError; end end # Else, just require it
@@ -11,16 +11,17 @@ module Sword
     # This piece of code is from Sinatra,
     # tweaked a bit to silent Thin server
     # and add Sword version and &c.
-      def run! port
+      def run!
+        port = $port || 1111
         handler = detect_rack_handler
         server_settings = settings.respond_to?(:server_settings) ? settings.server_settings : {}
         handler.run self, server_settings.merge(Port: port, Host: bind) do |server|
-          STDERR.print ">> Sword #{VERSION} at your service!\n" +
+          STDERR.print ">> Sword #{$settings[:version]} at your service!\n" +
           "   http://localhost:#{port} to see your project.\n" +
           "   CTRL+C to stop.\n"
           [:INT, :TERM].each { |s| trap(s) { quit! server, handler.name.gsub(/.*::/, '') } }
-          server.silent = true unless @debug
           server.threaded = settings.threaded
+          server.silent = true unless @debug
           set :running, true
           yield server if block_given?
         end
@@ -39,7 +40,7 @@ module Sword
     end
 
     disable :show_exceptions # show `error.erb`
-    set :views, @directory # Structure-agnostic
+    set :views, $directory || '.' # Structure-agnostic
     set :public_folder, settings.views
 
     error do
@@ -49,21 +50,24 @@ module Sword
 
     get('/favicon.ico') { send_file "#{$library}/favicon.ico" }
 
+    helpers do def parse thing, variable, to = nil, options = {}
+      return send_file "#{thing}.#{to}" if to and File.exists? "#{thing}.#{to}"
+      $settings[variable + 's'].map { |e| e.instance_of?(String) ? {e => [e]} : e }.each do |language|
+        language.each do |engine, from| from.each do |xxx|
+          # Iterate through extensions and find the engine you need.
+          return send engine, thing.to_sym, options if File.exists? "#{thing}.#{xxx}"
+        end end
+      end
+    end end
+
     get '/*.css' do |style|
-      return send_file "#{style}.css" if File.exists? "#{style}.css"
-      @settings['styles'].each do |k,v| v.each do |e| # for `extension`
-        # Iterate through extensions and find the engine you need.
-        return send k, style.to_sym, (compass || {}) if File.exists? "#{style}.#{e}"
-      end end
+      parse style, 'style', 'css', (compass || {})
       # If none, then raise an exception.
       raise 'Stylesheet not found'
     end
 
     get '/*.js' do |script|
-      return send_file "#{script}.js" if File.exists? "#{script}.js"
-      @settings['scripts'].each do |k,v| v.each do |e|
-        return send k, script.to_sym if File.exists? "#{script}.#{e}"
-      end end
+      parse script, 'script', 'js'
       raise 'Script not found'
     end
 
@@ -73,17 +77,13 @@ module Sword
     end
 
     get '/*/?' do |page|
-      %w[html htm].each do |e|
+      %w[html htm].each do |xxx|
         # This is specially for dumbasses who use .htm extension.
         # If you know another ultra-dumbass html extension, let me know.
-        return send_file "#{page}.#{e}" if File.exists? "#{page}.#{e}"
+        return send_file "#{page}.#{xxx}" if File.exists? "#{page}.#{xxx}"
       end
-      @settings['pages'].each do |k,v| v.each do |e|
-        # If Slim, then prettify the code so it is OK to read
-        return send k, page.to_sym, pretty: true if File.exists? "#{page}.#{e}"
-      end end
-      # Is it an index? Call it recursively.
-      raise "Page not found" if page =~ /index/
+      parse page, 'page', nil, {:pretty => true}
+      raise 'Page not found' if page =~ /index/
       call env.merge('PATH_INFO' => "/#{page}/index") 
     end
   end
