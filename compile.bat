eb4cf8f04838f6a303daffc2bd9f9cb1f81cb018
George <somu@so.mu>
2013-06-18 16:25:08 +0400

    100% Windows compatible

--- compile.bat
+++ compile.bat
@@ -1,1 +1,1 @@
-ocra bin/sword lib/* --gemfile Gemfile --icon icon.ico --add-all-core --console --no-dep-run --chdir-first
+ocra bin/sword lib/* --gemfile Gemfile --icon icon.ico --add-all-core --console --no-dep-run --gem-full
