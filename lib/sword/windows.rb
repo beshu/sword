53f8efc8bd544adbd9d2726746291e097b0c49c7
George <somu@so.mu>
2013-07-13 09:31:59 +0400

    boot and core folders, better windows.rb, more modules, more options, more SOLID

--- lib/sword/windows.rb
+++ lib/sword/windows.rb
@@ -1,17 +1,8 @@
+require 'rbconfig'
+
 module Sword
   module Windows
-    # Show options menu and then get options from STDIN
-    # 
-    # @param options [OptionParser] optparse object
-    # @return [Array] options received from STDIN
-    def self.get_options(options)
-      [:INT, :TERM].each { |s| trap(s) { abort "\n" } }
-      options.banner = 'Options (press ENTER if none):'
-      print options, "\n"
-      STDIN.gets.split
-    end
-
     # Check if we’re running Windows there:
-    WINDOWS = RUBY_PLATFORM =~ /mswin|windows|win32|mingw|cygwin/
+    WINDOWS = RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
   end
 end
