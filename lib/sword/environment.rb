e0669d20b90aef967c7a9d14ec23f3278c31a723
George <somu@so.mu>
2013-08-21 21:01:32 +0400

    And a little bit more refactoring

--- lib/sword/environment.rb
+++ lib/sword/environment.rb
@@ -11,21 +11,20 @@ module Sword
   Environment.configure do |e|
     e.directory = '.'
     e.templates = {}
-    e.gems = []
-    e.port = 1111
+    e.gems      = []
+    e.port      = 1111
+
+    e.home       = Dir.home if Dir.respond_to? :home
+    e.home     ||= ENV['HOME']
+    e.local_gems = "#{e.home}/.swordgems"
+    e.settings   = "#{e.home}/.swordrc"
 
     e.library = File.dirname File.dirname(__FILE__)
-    e.compass = e.library + '/compass.rb'
-    e.favicon = e.library + '/favicon.ico'
-    e.error   = e.library + '/error.erb'
+    e.compass = "#{e.library}/compass.rb"
+    e.favicon = "#{e.library}/favicon.ico"
+    e.error   = "#{e.library}/error.erb"
 
     e.template_lists = Dir["#{e.library}/templates/*.yml"]
     e.gem_lists      = Dir["#{e.library}/gems/*.yml"]
-
-    e.home = Dir.home if Dir.respond_to? :home
-    e.home ||= ENV['HOME']
-
-    e.local_gems = e.home + '/.swordgems'
-    e.settings   = e.home + '/.swordrc'
   end
 end
