f9de19278bd50ca1ede2a770de7b9a535713762f
George <somu@so.mu>
2013-08-21 19:33:00 +0400

    Fixing shadowing of outer variable

--- lib/sword/server/parsers.rb
+++ lib/sword/server/parsers.rb
@@ -27,7 +27,7 @@ module Sword
       def parse_templates
         parse @templates, '/*/?' do |app, page, env|
           files = HTML.dup.map { |extension| "#{Environment.directory}/#{page}.#{extension}" }
-          file = files.find { |file| File.exists? file }
+          file = files.find { |f| File.exists? f }
           if file
             app.erb File.read(file)
           else
