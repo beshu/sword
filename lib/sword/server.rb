685019266d806625dabb9a0f06b58b20eeebb221
George <somu@so.mu>
2013-10-24 16:20:03 +0400

    Cleaner get '*' implementation

--- lib/sword/server.rb
+++ lib/sword/server.rb
@@ -1,5 +1,5 @@
 require'hobbit';require'tilt';class Sword::Server<Hobbit::Base;def self.l;s=[];Dir['**/*'].each{|f|
 (t=Tilt[f]).nil?? s<<"/#{f}":get("/#{f.chomp File.extname f}#{Rack::Mime::MIME_TYPES.key m=
 t.default_mime_type}"){response.headers['Content-Type']="#{m};charset=utf-8";t.new(f).render}}
-use Rack::Static,:urls=>s;get('*'){$_==v=env['REQUEST_PATH']?($_=nil;raise LoadError):(
-(k=self.class).instance_eval{routes['GET']=[]};k.l);response.redirect$_=v}end;l end
+use Rack::Static,:urls=>s;get('*'){($v=nil;raise'NotFound')if$v==v=env['REQUEST_PATH']
+(k=self.class).instance_eval{routes['GET']=[]};k.l;response.redirect$v=v}end;l end
