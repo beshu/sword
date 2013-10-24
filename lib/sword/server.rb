f2d1b1afa8a38388b0eb71b988789266e7386009
George <somu@so.mu>
2013-10-24 18:08:45 +0400

    No #nil?

--- lib/sword/server.rb
+++ lib/sword/server.rb
@@ -1,7 +1,7 @@
 # Ladies and gentlemen, I bloody introduce you:
 #
 require'hobbit';require'tilt';class Sword::Server<Hobbit::Base;def self.l;s=[];Dir['**/*'].each{|f|
-(t=Tilt[f]).nil?? s<<"/#{f}":get("/#{f.chomp File.extname f}#{Rack::Mime::MIME_TYPES.key m=
-t.default_mime_type}"){response.headers['Content-Type']="#{m};charset=utf-8";t.new(f).render}}
-use Rack::Static,:urls=>s;get('*'){($v=nil;raise'NotFound')if$v==$v=env['REQUEST_PATH']
-(k=self.class).instance_eval{routes['GET']=[]};k.l;response.redirect$v}end;l end
+(t=Tilt[f])?get("/#{f.chomp File.extname f}#{Rack::Mime::MIME_TYPES.key m=t.default_mime_type}"){
+response.headers['Content-Type']="#{m};charset=utf-8";t.new(f).render}:s<<"/#{f}"};use Rack::Static,
+:urls=>s;get('*'){($v=nil;raise'NotFound')if$v==$v=env['REQUEST_PATH'];(k=self.class).instance_eval{
+routes['GET']=[]};k.l;response.redirect$v}end;l end
