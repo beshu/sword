af820e6110180e9d0c4319ad52c25769528b09e8
George <somu@so.mu>
2013-10-24 14:23:02 +0400

    #r → #s, $S → $_

--- lib/sword/server.rb
+++ lib/sword/server.rb
@@ -1,5 +1,5 @@
-require'hobbit';require'tilt';class Sword::Server<Hobbit::Base;def self.r;s=[];Dir['**/*'].each{|f|
+require'hobbit';require'tilt';class Sword::Server<Hobbit::Base;def self.l;s=[];Dir['**/*'].each{|f|
 (t=Tilt[f]).nil?? s<<"/#{f}":get("/#{f.chomp File.extname f}#{Rack::Mime::MIME_TYPES.key m=
 t.default_mime_type}"){response.headers['Content-Type']="#{m};charset=utf-8";t.new(f).render}}
-use Rack::Static,:urls=>s;get('*'){$S==v=env['REQUEST_PATH']?($S=nil;raise LoadError):(
-(k=self.class).instance_eval{routes['GET']=[]};k.r);response.redirect$S=v}end;r end
+use Rack::Static,:urls=>s;get('*'){$_==v=env['REQUEST_PATH']?($_=nil;raise LoadError):(
+(k=self.class).instance_eval{routes['GET']=[]};k.l);response.redirect$_=v}end;l end
