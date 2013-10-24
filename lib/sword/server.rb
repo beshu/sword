6e24cc34debecac760e176dea921f9495c688abc
George <somu@so.mu>
2013-10-24 13:53:06 +0400

    No M constant, using Rack::Mime::MIME_TYPES

--- lib/sword/server.rb
+++ lib/sword/server.rb
@@ -1,6 +1,5 @@
-require'hobbit';require'tilt';class Sword::Server<Hobbit::Base;M={'application/javascript'=>
-'js','text/css'=>'css','text/html'=>'html'};def self.r;s=[];Dir['**/*'].each{|f|(t=Tilt[f]).nil??
-s<<"/#{f}":get("/#{f.chomp File.extname f}.#{M[m=t.default_mime_type]}"){response.headers[
-'Content-Type']="#{m};charset=utf-8";t.new(f).render}};use Rack::Static,:urls=>s;get('*'){$S==v=
-env['REQUEST_PATH']?($S=nil;raise LoadError):((k=self.class).instance_eval{routes['GET']=[]};k.r)
-response.redirect$S=v}end;r end
+require'hobbit';require'tilt';class Sword::Server<Hobbit::Base;def self.r;s=[];Dir['**/*'].each{|f|
+(t=Tilt[f]).nil?? s<<"/#{f}":get("/#{f.chomp File.extname f}#{Rack::Mime::MIME_TYPES.key m=
+t.default_mime_type}"){response.headers['Content-Type']="#{m};charset=utf-8";t.new(f).render}}
+use Rack::Static,:urls=>s;get('*'){$S==v=env['REQUEST_PATH']?($S=nil;raise LoadError):(
+(k=self.class).instance_eval{routes['GET']=[]};k.r);response.redirect$S=v}end;r end
