089da39ff4e93838389eb24d0dc37b2d5f5955db
George <somu@so.mu>
2013-10-24 16:56:41 +0400

    Ladies and gentlemen

--- lib/sword/server.rb
+++ lib/sword/server.rb
@@ -1,3 +1,5 @@
+# Ladies and gentlemen, I fucking introduce you my static web-server with built-in preprocessing:
+#
 require'hobbit';require'tilt';class Sword::Server<Hobbit::Base;def self.l;s=[];Dir['**/*'].each{|f|
 (t=Tilt[f]).nil?? s<<"/#{f}":get("/#{f.chomp File.extname f}#{Rack::Mime::MIME_TYPES.key m=
 t.default_mime_type}"){response.headers['Content-Type']="#{m};charset=utf-8";t.new(f).render}}
