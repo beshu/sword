3262c67b2b1f3e16a0f00837d619218e74995d04
George <somu@so.mu>
2013-11-06 21:03:52 +0400

    Sword::CLI (fuck yeah)

--- lib/sword/cli.rb
+++ lib/sword/cli.rb
@@ -1,1 +1,34 @@
-Rack::Handler.default.run(Sword.new, :Port => 1111)
+require 'sword'
+
+class Sword::CLI
+  @@initializers = []
+
+  def initialize(arguments = ARGV)
+    @arguments = arguments
+    @@initializers.each { |i| instance_eval(&i) }
+  end
+
+  def self.method_missing(_,&block)
+    @@initializers << Proc.new(&block)
+  end
+
+  parse_options do
+    require 'sword/tuner'
+    @options = Sword::Tuner.new(@arguments)
+  end
+
+  filter_rack_options do
+    @rack = @options.select { |o,_| /[A-Z]/ =~ o[0] }
+    @options.delete(@rack)
+  end
+
+  change_directory do
+    if directory = @options.delete(:directory)
+      require 'fileutils'
+      FileUtils.cd(directory)
+    end
+  end
+
+  inject_routes { Sword.class_eval { l } }
+  start_server  { Rack::Handler.default.run(Sword.new, @rack) }
+end
