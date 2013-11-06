4e655d88a2040788355fef2f82a84939670f02d0
George <somu@so.mu>
2013-11-06 21:03:44 +0400

    Something has changed, I don’t remember what

--- lib/sword.rb
+++ lib/sword.rb
@@ -1,5 +1,5 @@
 require'hobbit';require'tilt';class Sword<Hobbit::Base;def self.l;s=[];Dir['**/*'].each{|f|
 (t=Tilt[f])?get("/#{f.chomp File.extname f}#{Rack::Mime::MIME_TYPES.key m=t.default_mime_type}"){
 response.headers['Content-Type']="#{m};charset=utf-8";t.new(f).render}:s<<"/#{f}"};use Rack::Static,
-:urls=>s;get('*'){($v=nil;raise'NotFound')if$v==$v=env['REQUEST_PATH'];(k=self.class).instance_eval{
-routes['GET']=[]};k.l;response.redirect$v}end;l end
+:urls=>s;get('*'){($v=nil;throw:N)if$v==$v=env['REQUEST_PATH'];(k=self.class).instance_eval{
+routes['GET']=[]};k.l;response.redirect$v}end end
