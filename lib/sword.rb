f57ff75be5dd14f2c321e58d5b33f86cfefcd5c6
George <somu@so.mu>
2013-11-08 16:17:36 +0400

    Now the limit is 80 chars per row, some pieces refactored

--- lib/sword.rb
+++ lib/sword.rb
@@ -1,6 +1,7 @@
-require'hobbit';require'tilt';class Sword<Hobbit::Base;def self._;s=[];Dir['**/*'].each{|f|(t=Tilt[
-f])?get("/#{f.chomp File.extname f}#{(@@r=Rack::Mime::MIME_TYPES).key m=t.default_mime_type}"){(@@h=
-lambda{|m|response.headers['Content-Type']=m}).call m;t.new(f).render}:s<<"/#{f}"};use Rack::Static,
-:urls=>s;get(i="/favicon.ico"){@@h.call@@r['.ico'];File.read"#{File.dirname __FILE__}#{i}"};get('*'
-){(@@p=0;raise)if(@@p||=0)==@@p=env['REQUEST_PATH'];(k=self.class).instance_eval{routes['GET']=[]}
-k._;response.redirect@@p}end end
+require'hobbit';require'tilt';class Sword<Hobbit::Base;def self._;c,o,s=lambda{|
+m,s|s.response.headers['Content-Type']=m},Rack::Mime::MIME_TYPES,[];Dir['**/*'].
+each{|f|(t=Tilt[f])?get("/#{f.chomp File.extname f}#{o.key m=t.default_mime_type
+}"){c.call m,self;t.new(f).render}:s<<"/#{f}"};use Rack::Static,:urls=>s;get(i=
+"/favicon.ico"){c.call o['.ico'],self;File.read"#{File.dirname __FILE__}#{i}"}
+get('*'){(raise@@p=MethodError)if(@@p||=0)==@@p=env['REQUEST_PATH'];(k=self.
+class).instance_eval{routes['GET']=[]};k._;response.redirect@@p}end end
