7460a90abb43edceebddc71fab860c1e6ef076e5
George <somu@so.mu>
2013-11-08 17:03:50 +0400

    MethodError → Error, z='.ico'

--- lib/sword.rb
+++ lib/sword.rb
@@ -2,6 +2,6 @@ require'hobbit';require'tilt';class Sword<Hobbit::Base;def self._;c,o,s=lambda{|
 x,y|y.response.headers['Content-Type']=x},Rack::Mime::MIME_TYPES,[];Dir['**/*'].
 each{|f|(t=Tilt[f])?get("/#{f.chomp File.extname f}#{o.key m=t.default_mime_type
 }"){c.call m,self;t.new(f).render}:s<<"/#{f}"};use Rack::Static,:urls=>s;get(i=
-"/favicon.ico"){c.call o['.ico'],self;File.read"#{File.dirname __FILE__}#{i}"}
-get('*'){(raise@@p=MethodError)if(@@p||=0)==@@p=env['REQUEST_PATH'];(k=self.
-class).instance_eval{routes['GET']=[]};k._;response.redirect@@p}end end
+'/favicon'<<z='.ico'){c.call o[z],self;File.read"#{File.dirname __FILE__}#{i}"}
+get('*'){(raise@@p=Error)if(@@p||=0)==@@p=env['REQUEST_PATH'];(k=self.class).
+instance_eval{routes['GET']=[]};k._;response.redirect@@p}end end
