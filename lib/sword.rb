62b4c2a62a414d82a72670771a13cabc222efb26
George <somu@so.mu>
2013-11-14 20:48:37 +0400

    Index page handling

--- lib/sword.rb
+++ lib/sword.rb
@@ -1,7 +1,9 @@
-require'hobbit';require'tilt';class Sword<Hobbit::Base;def self._;c,o,s=lambda{|
-x,y|y.response.headers['Content-Type']=x},Rack::Mime::MIME_TYPES,[];Dir['**/*'].
-each{|f|(t=Tilt[f])?get("/#{f.chomp File.extname f}#{o.key m=t.default_mime_type
-}"){c[m,self];t.new(f).render}:s<<"/#{f}"};use Rack::Static,:urls=>s;get(i=
-'/favicon'<<z='.ico'){c[o[z],self];File.read"#{File.dirname __FILE__}#{i}"}
-get('*'){(raise@@p='NOT FOUND')if(@@p||=0)==@@p=env['REQUEST_PATH'];(k=self.
-class).instance_eval{routes['GET']=[]};k._;response.redirect@@p}end end
+require'hobbit';require'tilt';class Sword<Hobbit::Base;def self._;s,c,o,@@t=[],
+lambda{|x,y|y.response.headers['Content-Type']=x},Rack::Mime::MIME_TYPES.invert,
+0;Dir['**/*'].each{|f|(t=Tilt[f])?get("/#{f.chomp File.extname f}#{o[m=t.
+default_mime_type]}"){c[m,self];t.new(f).render}:s<<"/#{f}"};use Rack::Static,
+:urls=>s;get(i='/favicon'<<z='.ico'){c[o.key(z),self];File.read"#{File.dirname
+__FILE__}#{i}"};get('*'){|e|@@t<2?e.instance_eval{@@t+=1;@response=call(@env.
+merge p='PATH_INFO'=>"#{@env[p].chomp'/'}/index.html")[-1];@@t=0}:((@@t=0
+raise@@p='NOT FOUND')if(@@p||=0)==@@p=env['REQUEST_PATH'];(k=e.class).
+instance_eval{routes['GET']=[]};k._;response.redirect@@p)}end end
