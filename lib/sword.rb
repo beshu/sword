9fa11462b7c6d819c9661893b5fcf92e463fd437
George <somu@so.mu>
2014-01-17 00:14:57 +0400

    80 chars per line and refactored

--- lib/sword.rb
+++ lib/sword.rb
@@ -1,12 +1,13 @@
-require'tilt';require'rack';require'forwardable';class Sword;M=(R=Rack)::Mime::MIME_TYPES.
-invert;P='PATH_INFO';class<<self;extend Forwardable;def_delegators:stack,:map,:use;def g(
-p,&b);r[p]=b;end;alias:m:new;def new;@s.run m;@s;end;def r;@r||={};end;def stack;@s||=R::
-Builder.new;end;def w;@r={};_;end;def _;s,@@t=[],0;Dir['**/*'].each{|f|(t=Tilt[f])?g("/#{
-f.chomp File.extname f}#{M[m=t.default_mime_type]}"){q m;t.new(f).render}:s<<"/#{f}"};use(
-R::Static,:urls=>s);g(i='/favicon'<<z='.ico'){q M.key z;File.read"#{File.dirname __FILE__
-}#{i}"};g(:a){|e|@@t<2?e.instance_eval{@@t+=1;@c=call(@a.merge P=>"#{@a[P].chomp'/'
-}/index.html")[-1];@@t=0}:((@@t=0;raise@@p='NOT FOUND')if(@@p||=0)==@@p=a['REQUEST_PATH'
-];e.class.w;c.redirect@@p)}end end;attr_reader:a,:b,:c;def q(t);@c.headers['Content-Type'
-]=t;end;def call(a);a[P]='/'if a[P].empty?;@a=a;@b=R::Request.new@a;@c=R::Response.new;l
-@c.finish;end;def l;l=(j=self.class.r).detect{|r,_|r==b.path_info};c.write instance_eval(
-&l ?l[-1]:j[:a])end end
+require'tilt';require'rack';require'forwardable';class Sword;M=(R=Rack)::Mime::
+MIME_TYPES.invert;P='PATH_INFO';class<<self;extend Forwardable;def_delegators(
+:stack,:map,:use);def g(p,&b);r[p]=b;end;alias:m:new;def new;@s.run m;@s end;def
+r;@r||={};end;def stack;@s||=R::Builder.new;end;def w;@r={};_ end;def _;@@t,s=0,
+[];Dir['**/*'].each{|f|(t=Tilt[f])?g("/#{f.chomp File.extname f}#{M[m=t.
+default_mime_type]}"){q m;t.new(f).render(self)}:s<<"/#{f}"};use R::Static,
+:urls=>s;g(i='/favicon'<<z='.ico'){q M.key z;File.read"#{File.dirname __FILE__
+}#{i}"}end end;def q t;@c.headers['Content-Type']=t;end;def call a;a[P]='/'if a[
+P].empty?;@a=a;@b=R::Request.new@a;@c=R::Response.new;l;@c.finish;end;def l;l=
+Sword.r.detect{|r,_|r==@b.path_info};@c.write instance_eval(&l ?l[-1]:proc{|e|
+@@t<2?e.instance_eval{@@t+=1;@c=call(@a.merge P=>"#{@a[P].chomp'/'}/index.html"
+)[-1];@@t=0}:((@@t=0;raise@@p='NOT FOUND')if(@@p||=0)==@@p=@a['REQUEST_PATH'];e.
+class.w;@c.redirect@@p)})end end
