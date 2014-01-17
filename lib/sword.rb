cb59a95b771a1b43d8f76a32e4466e4c0bbc6fa9
George <somu@so.mu>
2014-01-17 04:52:19 +0400

    Revamped rest-case proc
    Much faster, almost no heuristics

--- lib/sword.rb
+++ lib/sword.rb
@@ -1,11 +1,11 @@
 require'tilt';require'rack';class Sword;M=(R=Rack)::Mime::MIME_TYPES.invert;P=
-'PATH_INFO';class<<self;def g(p,&b);r[p]=b;end;alias:m:new;def new;x.run m;x end
-def r;@r||={};end;def x;@x||=R::Builder.new;end;def w;@r.clear;@x.instance_eval{
-@use.clear};_ end;def _;@@t,s=0,[];Dir['**/*'].each{|f|(t=Tilt[f])?g("/#{f.chomp(
-File.extname f)}#{M[m=t.default_mime_type]}"){q m;t.new(f).render(self)}:s<<"/#{
-f}"};x.use(R::Static,:urls=>s);g(i='/favicon'<<z='.ico'){q M.key z;File.read"#{
-File.dirname __FILE__}#{i}"}end end;def q t;@c.headers['Content-Type']=t end;def
-call a;@a=a;@b=R::Request.new@a;@c=R::Response.new;l=Sword.r[@b.path_info];@c.
-write instance_eval(&l ?l:proc{|e|@@t<2?e.instance_eval{@@t+=1;@a[P]="#{@a[P].
-chomp'/'}/index.html";@c=call(@a)[-1];@@t=0}:((@@t=0;raise@@p='NOT FOUND')if(
-@@p||=0)==@@p=@a['REQUEST_PATH'];Sword.w;@c.redirect@@p)});@c.finish end end
+'PATH_INFO';class<<self;def g(p,&b);@@r[p]=b;end;alias:m:new;def new;@@x.run m
+@@x end;@@r||={};@@x=R::Builder.new;def w;@@r.clear;@@x.instance_eval{@use.clear
+};_ end;def _;@@s=[];Dir['**/*'].each{|f|(t=Tilt[f])?g("/#{f.chomp File.extname(
+f)}#{M[m=t.default_mime_type]}"){q m;t.new(f).render(self)}:@@s<<"/#{f}"};@@x.
+use(R::Static,:urls=>@@s);g(i='/favicon'<<z='.ico'){q M.key z;File.read"#{File.
+dirname __FILE__}#{i}"}end end;def q t;@c.headers['Content-Type']=t;end;def
+call a;@a=a;@b=R::Request.new@a;@c=R::Response.new;l=@@r[@b.path_info];@c.write(
+instance_eval(&l ?l:proc{|e|@@r[i="#{(n=@a[P]).chomp'/'}/index.html"]?call(@a.
+merge P=>i)[-1]:(Sword.w;@@r[n]||@@s.include?(n)?@c.redirect(n):'not found')}))
+@c.finish end end
