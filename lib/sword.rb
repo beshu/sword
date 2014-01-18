af409105477186aaeec34ee233ecbe34a97355b4
George <somu@so.mu>
2014-01-18 08:16:31 +0400

    Basic 404

--- lib/sword.rb
+++ lib/sword.rb
@@ -1,10 +1,11 @@
 require'tilt';require'rack';class Sword;M=(R=Rack)::Mime::MIME_TYPES.invert;P=
-'PATH_INFO';class<<self;def g(p,&b);@@r[p]=b;end;alias:n:new;def new;@@x.run n
-@@x end;@@x=R::Builder.new;def _;@@s,@@r=[],{};Dir['**/*'].each{|f|(t=Tilt[f])?
-g("/#{f.chomp File.extname f}#{M[m=t.default_mime_type]}"){q m;t.new(f).render(
-self)}:@@s<<"/#{f}"};@@x.use(R::Static,:urls=>@@s);g(i='/favicon'<<z='.ico'){q
-M.key z;File.read"#{File.dirname __FILE__}#{i}"}end end;def q t;@c.headers[
+'PATH_INFO';D=File.dirname __FILE__;class<<self;def g(p,&b);@@r[p]=b;end;alias:n
+:new;def new;@@x.run n;@@x end;@@x=R::Builder.new;def _;@@s,@@r=[],{};Dir['**/*'
+].each{|f|(t=Tilt[f])?g("/#{f.chomp File.extname f}#{M[m=t.default_mime_type]}"
+){q m;t.new(f).render(self)}:@@s<<"/#{f}"};@@x.use(R::Static,:urls=>@@s);g(i=
+'/favicon'<<z='.ico'){q M.key z;File.read"#{D}#{i}"}end end;def q t;@c.headers[
 'Content-Type']=t;end;def call a;@a=a;@b=R::Request.new@a;@c=R::Response.new;l=
 @@r[@b.path_info];@c.write instance_eval(&l ?l:proc{|e|@@r[i="#{(n=@a[P]).chomp(
 '/')}/index.html"]?call(@a.merge P=>i)[-1]:(@@x.instance_eval{@use.clear};Sword.
-_;@@r[n]||@@s.include?(n)?@c.redirect(n):'not found')});@c.finish end end
+_;@@r[n]||@@s.include?(n)?@c.redirect(n):Tilt.new("#{D}/404.erb").render(n))})
+@c.finish end end
