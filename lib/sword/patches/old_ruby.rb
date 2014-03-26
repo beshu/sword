b59843681fde1d6ee4c128fd70df41342c3c2f44
George <somu@so.mu>
2014-03-26 17:55:08 +0400

    Indentation

--- lib/sword/patches/old_ruby.rb
+++ lib/sword/patches/old_ruby.rb
@@ -2,8 +2,8 @@ Sword::Patch.new do
   if RUBY_VERSION < '1.9'
     # Ruby 1.8's hashes are unordered, and that
     # makes htm/html extension inconsistent for
-    # each run. This is especially important for
-    # --compile flag support.
+    # each run. This fix is especially important
+    # for --compile flag support.
     Rack::Mime::MIME_TYPES.delete('.htm')
   end
 end
